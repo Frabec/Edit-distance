9dcbe0999b8a549a2ce8e152b260ef736130f7c6
Franco Rabec <franco.rabec@hotmail.fr>
2017-12-07 21:12:21 +0100

    version buggée

--- EditDistance.java
+++ EditDistance.java
@@ -12,6 +12,117 @@ public class EditDistance implements EditDistanceInterface {
 		this.c_r = c_r;
 	}
 
+	public int getEditDistanceDPminimal(String s1, String s2) {
+		int m = s1.length();
+		int n = s2.length();
+		int[][] memoize = new int[m+1][n+1]; // array where memoized values are stored
+		for (int i=0; i<m+1;i++){
+			for (int j=0; j<n+1; j++){
+				memoize[i][j]=-1;
+			}
+		}
+		for (int i = 0; i < m + 1; i++) { // initialisation of memoize
+			memoize[i][0] = i*this.c_d;
+		}
+		for (int j =0;j<n+1;j++){
+			memoize[0][j] = j*this.c_i;
+		}
+		
+		for (int k =2; k<=m+n;k++){
+			for (int j = Math.max(1, k-m); j<=Math.min(k-1,n); j++){
+				int i=k-j;
+				if (s1.charAt(i-1)==s2.charAt(j-1)){
+					memoize[i][j]=Math.min((memoize[i-1][j-1]), Math.min(memoize[i-1][j]+this.c_d,memoize[i][j-1]+this.c_i));
+				}
+				else {
+					memoize[i][j]=Math.min((memoize[i-1][j-1]+this.c_r), Math.min(memoize[i-1][j]+this.c_d,memoize[i][j-1]+this.c_i));
+				}
+				
+			}
+		}
+		return (memoize[m][n]);
+	}
+	
+	public int[][] getEditDistanceDP(String s1, String s2,int x) {
+		int m = s1.length();
+		int n = s2.length();
+		int[][] memoize = new int[m+1][n+1]; // array where memoized values are stored
+		for (int i=0; i<m+1;i++){
+			for (int j=0; j<n+1; j++){
+				if (Math.abs(i-j) >Math.abs(n-m)+x){
+					memoize[i][j]=MAX;
+				}
+			}
+		}
+		for (int i = 0; i < m + 1; i++) { // initialisation of memoize
+			memoize[i][0] = i*this.c_d;
+		}
+		for (int j =0;j<n+1;j++){
+			memoize[0][j] = j*this.c_i;
+		}
+		
+		for (int k =2; k<=m+n;k++){
+			for (int j = Math.max(1, k-m); j<=Math.min(k-1,n); j++){
+				int i=k-j;
+				if (memoize[i][j]==MAX){
+					continue;
+				}
+				if (s1.charAt(i-1)==s2.charAt(j-1)){
+					memoize[i][j]=Math.min((memoize[i-1][j-1]), Math.min(memoize[i-1][j]+this.c_d,memoize[i][j-1]+this.c_i));
+				}
+				else {
+					memoize[i][j]=Math.min((memoize[i-1][j-1]+this.c_r), Math.min(memoize[i-1][j]+this.c_d,memoize[i][j-1]+this.c_i));
+				}
+				
+			}
+		}
+		return (memoize);
+	}
+
+	public List<String> getMinimalEditSequence(String s1, String s2) {
+		int x=1;
+		int i=s1.length()-1;
+		int j=s2.length()-1;
+		String current=s2;
+		int[][] tab;
+		int trueValue= getEditDistanceDPminimal(s1, s2);
+		LinkedList<String> path = new LinkedList<String>();
+		while (true){
+			tab=getEditDistanceDP(s1, s2, x);
+			if (tab[s1.length()][s2.length()]==trueValue){
+				break;
+			}
+			x++;
+		}
+		//we find the path
+		while (true){
+			if (i==0){
+				return path;
+			}
+			if (tab[i][j]==tab[i-1][j-1]){
+				i--;
+				j--;
+			}
+			else if (tab[i][j]==tab[i-1][j-1]+this.c_r){
+				path.addFirst("replace("+i+","+current.charAt(i)+")");
+				i--;
+				j--;
+			}
+			else if (tab[i][j]==tab[i-1][j]+this.c_d){
+				path.addFirst("delete"+"("+i+")");
+				current=current.substring(0,i+1)+"a"+current.substring(i,current.length());
+				i--;
+				
+			}
+			else if (tab[i][j]==tab[i][j-1]+this.c_i){
+				current=current.substring(0,i)+current.substring(i,current.length());
+				path.addFirst("insert("+"("+i+","+current.charAt(i)+")");
+				j--;
+			}
+		}
+	}
+
+	
 	public int[][] getEditDistanceDP(String s1, String s2) {
 		int m = s1.length();
 		int n = s2.length();
@@ -41,21 +152,5 @@ public class EditDistance implements EditDistanceInterface {
 			}
 		}
 		return (memoize);
-	}
-
-	public List<String> getMinimalEditSequence(String s1, String s2) {
-		/* To be completed in Part 2. Remove sample code block below. */
-		LinkedList<String> ls = new LinkedList<>();
-		if (c_r == 6) {
-			ls.add("delete(1)");
-			ls.add("delete(1)");
-			ls.add("insert(2,c)");
-			ls.add("insert(3,b)");
-		} else {
-			ls.add("replace(1,d)");
-			ls.add("replace(3,b)");
-		}
-		return ls;
-		/* Code block to be removed ends. */
 	}
 };
